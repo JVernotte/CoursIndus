4c657ad2eb1241ca498f4a97306d3922da83e6ee
Jvernotte <j.vernotte@it-students.fr>
2020-06-15 10:09:14 +0200

    first commit

---
+++ Addition.py
@@ -0,0 +1,10 @@
+# Ce programme additionne deux nombres
+
+num1 = 1.5
+num2 = 6.3
+
+# addition
+sum = num1 + num2
+
+# affiche le resultat
+print('La somme de {0} et {1} est {2}'.format(num1, num2, sum))
